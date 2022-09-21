2c06039b03bbb5d897d0c69e0d593865ea6c4b75
Rupam Das <rejisterjack@gmail.com>
2022-09-21 19:28:22 +0530

    updated day 2

--- Node Assignment 2/server.js
+++ Node Assignment 2/server.js
@@ -5,8 +5,10 @@ const port = process.env.port || 4000
 app.get("/",(req, res)=>{
   res.status(200).send(`<h1>express server running</h1>`)
 })
-app.get("/login",(req, res)=>{
-  res.status(200).send(`<h1>login page</h1>`)
+app.get("/api/main",(req, res)=>{
+  res.status(200).send(`<h1>Express JS</h1>
+  <p>Express.js, or simply Express, is a back end web application framework for build RESTful APIs with Node.js</p>
+  <p>t is designed for building web applications and APIs</p>`)
 })
 app.listen(port,()=>{
   console.log(`server is running on port ${port}`);
